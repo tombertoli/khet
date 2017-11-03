fd6da46847fee400b675fb2d3803f9d52d095989
bertoli tomas <43172380@ort>
2017-11-03 15:56:01 -0300

    Se sigue con el lobby.

---
+++ src/khet/Assets/Scripts/UI/Connect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Connect : MonoBehaviour {
+	[SerializeField] private InputField iField;
+	[SerializeField] private HUDController controller;
+	private Button button;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button>();
+		button.onClick.AddListener(() => controller.Connect(iField.text));
+		button = null;
+	}
+}
